84a50d9ee7c78746e96422158345ff4d4c7d6795
Anton O <mavarazy@gmail.com>
2013-02-28 22:37:29 +0200

    trash

---
+++ web-client/src/main/java/com/socialone/social/SimpleSignInAdapter.java
@@ -0,0 +1,18 @@
+package com.socialone.social;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.social.connect.Connection;
+import org.springframework.social.connect.web.SignInAdapter;
+import org.springframework.web.context.request.NativeWebRequest;
+
+public final class SimpleSignInAdapter implements SignInAdapter {
+
+    private final UserCookieGenerator userCookieGenerator = new UserCookieGenerator();
+
+    public String signIn(String userId, Connection<?> connection, NativeWebRequest request) {
+        userCookieGenerator.addCookie(userId, request.getNativeResponse(HttpServletResponse.class));
+        return null;
+    }
+
+}
