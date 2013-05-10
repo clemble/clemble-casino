ea5b2c8e1106263e243db9fc3f0ed6d432d782d1
Anton O <mavarazy@gmail.com>
2013-05-10 14:08:04 +0300

    some scripts added

--- web-client/src/main/webapp/WEB-INF/resources/assets/js/gogomaya-client.js
+++ web-client/src/main/webapp/WEB-INF/resources/assets/js/gogomaya-client.js
@@ -1,7 +1,50 @@
-$.get(
-    "somepage.php",
-    {paramOne : 1, paramX : 'abc'},
-    function(data) {
-       alert('page content: ' + data);
-    }
-);
+(function() {
+	select = function(selectedCell) {
+		$.ajax({
+			headers : {
+				"Content-Type" : "application/json",
+				"Accept" : "application/json",
+				"playerId" : localStorage.getItem('playerId'),
+				"tableId" : localStorage.getItem('tableId'),
+				"sessionId" : localStorage.getItem('sessionId')
+			},
+			type : "POST",
+			url : "http://gogomaya.cloudfoundry.com/spi/active/action",
+			data : JSON.stringify({
+				type : "select",
+				playerId : localStorage.getItem('playerId'),
+				cell : selectedCell
+			}, null, 2),
+			success : function(data, textStatus, jqXHR) {
+				alert("Success: " + data);
+			},
+			error : function(data) {
+				alert("Error " + JSON.stringify(data, null, 2));
+			}
+		});
+	}
+	bet = function(amount) {
+		$.ajax({
+			headers : {
+				"Content-Type" : "application/json",
+				"Accept" : "application/json",
+				"playerId" : localStorage.getItem('playerId'),
+				"tableId" : localStorage.getItem('tableId'),
+				"sessionId" : localStorage.getItem('sessionId')
+			},
+			type : "POST",
+			url : "http://gogomaya.cloudfoundry.com/spi/active/action",
+			data : JSON.stringify({
+				type : "bet",
+				playerId : localStorage.getItem('playerId'),
+				bet : amount
+			}, null, 2),
+			success : function(data, textStatus, jqXHR) {
+				alert("Success: " + data);
+			},
+			error : function(data) {
+				alert("Error " + JSON.stringify(data, null, 2));
+			}
+		});
+	}
+})
