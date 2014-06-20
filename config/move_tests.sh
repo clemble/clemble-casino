de52dba2801b40ba3dea6cd782a0b84e322983a1
Anton O <mavarazy@gmail.com>
2014-06-20 10:31:42 +0300

    Changing move script

--- config/move_tests.sh
+++ config/move_tests.sh
@@ -1,26 +1,27 @@
 #!/bin/sh
 mkdir -p ./shippable/testresults
-cp ./public/api/api-common/target/surefire-reports/* ./shippable/testresults/
-cp ./public/api/api-test/target/surefire-reports/* ./shippable/testresults/
-cp ./public/client/client-common/target/surefire-reports/* ./shippable/testresults/
-cp ./public/client/client-android/target/surefire-reports/* ./shippable/testresults/
+cp -R ./public/api/api-common/target/surefire-reports/* ./shippable/testresults/
+cp -R ./public/api/api-test/target/surefire-reports/* ./shippable/testresults/
 
-cp ./server/common/target/surefire-reports/* ./shippable/testresults/
-cp ./server/common-web/target/surefire-reports/* ./shippable/testresults/
+#cp -R ./public/client/client-common/target/surefire-reports/* ./shippable/testresults/
+cp -R ./public/client/client-android/target/surefire-reports/* ./shippable/testresults/
 
-cp ./server/game/game-common/target/surefire-reports/* ./shippable/testresults/
-cp ./server/game/game-management/target/surefire-reports/* ./shippable/testresults/
-cp ./server/game/game-web/target/surefire-reports/* ./shippable/testresults/
+cp -R ./server/common/target/surefire-reports/* ./shippable/testresults/
+cp -R ./server/common-web/target/surefire-reports/* ./shippable/testresults/
 
-cp ./server/payment/payment-common/target/surefire-reports/* ./shippable/testresults/
-cp ./server/payment/payment-management/target/surefire-reports/* ./shippable/testresults/
-cp ./server/payment/payment-web/target/surefire-reports/* ./shippable/testresults/
+#cp -R ./server/game/game-common/target/surefire-reports/* ./shippable/testresults/
+cp -R ./server/game/game-management/target/surefire-reports/* ./shippable/testresults/
+cp -R ./server/game/game-web/target/surefire-reports/* ./shippable/testresults/
 
-cp ./server/player/player-common/target/surefire-reports/* ./shippable/testresults/
-cp ./server/player/player-management/target/surefire-reports/* ./shippable/testresults/
-cp ./server/player/player-social/target/surefire-reports/* ./shippable/testresults/
-cp ./server/player/player-web/target/surefire-reports/* ./shippable/testresults/
+#cp -R ./server/payment/payment-common/target/surefire-reports/* ./shippable/testresults/
+cp -R ./server/payment/payment-management/target/surefire-reports/* ./shippable/testresults/
+cp -R ./server/payment/payment-web/target/surefire-reports/* ./shippable/testresults/
 
-cp ./integration/integration-base/target/surefire-reports/* ./shippable/testresults/
-cp ./integration/integration-game/target/surefire-reports/* ./shippable/testresults/
-cp ./integration/integration-test/target/surefire-reports/* ./shippable/testresults/
+cp -R ./server/player/player-common/target/surefire-reports/* ./shippable/testresults/
+cp -R ./server/player/player-management/target/surefire-reports/* ./shippable/testresults/
+cp -R ./server/player/player-social/target/surefire-reports/* ./shippable/testresults/
+cp -R ./server/player/player-web/target/surefire-reports/* ./shippable/testresults/
+
+#cp -R ./integration/integration-base/target/surefire-reports/* ./shippable/testresults/
+#cp -R ./integration/integration-game/target/surefire-reports/* ./shippable/testresults/
+cp -R ./integration/integration-test/target/surefire-reports/* ./shippable/testresults/
