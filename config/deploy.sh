f32a91e29735c1ec8df9a884b274281e6a34c86c
Anton O <mavarazy@gmail.com>
2014-06-24 05:56:47 +0300

    Restoring removed constraint

--- config/deploy.sh
+++ config/deploy.sh
@@ -2,4 +2,4 @@
 
 pip install awscli
 
-aws s3 cp server/player/player-web/target/player-web-0.9.0-SNAPSHOT.war s3://s3-us-west-2.amazonaws.com/clemble/player/player-web-0.9.0-SNAPSHOT.war
+aws s3 cp ./server/player/player-web/target/player-web-0.9.0-SNAPSHOT.war s3://s3-us-west-2.amazonaws.com/clemble/player/player-web-0.9.0-SNAPSHOT.war
