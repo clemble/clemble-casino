a682c456ab69b6dc1ef1c50608a70236a17d5d68
Anton O <mavarazy@gmail.com>
2014-07-05 13:43:34 +0300

    Refactoring configuration

---
+++ server/player/player-profile/src/main/java/com/clemble/casino/server/spring/player/PlayerProfileWebSpringConfiguration.java
@@ -0,0 +1,14 @@
+package com.clemble.casino.server.spring.player;
+
+import com.clemble.casino.server.spring.common.SpringConfiguration;
+import com.clemble.casino.server.spring.web.WebCommonSpringConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+/**
+ * Created by mavarazy on 7/5/14.
+ */
+@Configuration
+@Import({PlayerProfileSpringConfiguration.class, WebCommonSpringConfiguration.class})
+public class PlayerProfileWebSpringConfiguration implements SpringConfiguration {
+}
