b7c8e6e1258c598f5caa07e3e86c7543a2478547
Anton O <mavarazy@gmail.com>
2014-08-19 21:43:14 +0300

    Missed One WebSpringConfiguration

--- integration/integration-game/src/main/java/com/clemble/casino/integration/game/spring/IntegrationGameApplicationInitialization.java
+++ integration/integration-game/src/main/java/com/clemble/casino/integration/game/spring/IntegrationGameApplicationInitialization.java
@@ -5,7 +5,7 @@ import com.clemble.casino.server.spring.AbstractWebApplicationInitializer;
 public class IntegrationGameApplicationInitialization extends AbstractWebApplicationInitializer {
 
     public IntegrationGameApplicationInitialization() {
-        super(IntegrationGameWebSpringConfiguration.class);
+        super(IntegrationGameSpringConfiguration.class);
     }
 
 }
