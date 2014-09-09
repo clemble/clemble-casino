731475ba750e22750d5416f7d8097f33266237ca
Anton O <mavarazy@gmail.com>
2014-09-09 22:55:53 +0300

    Send default configurations for ConfigController

--- server/goal/goal-configuration/src/main/java/com/clemble/casino/goal/configuration/controller/GoalConfigurationServiceController.java
+++ server/goal/goal-configuration/src/main/java/com/clemble/casino/goal/configuration/controller/GoalConfigurationServiceController.java
@@ -1,15 +1,27 @@
 package com.clemble.casino.goal.configuration.controller;
 
+import com.clemble.casino.bet.Bid;
 import com.clemble.casino.goal.configuration.GoalConfiguration;
 import com.clemble.casino.goal.configuration.GoalConfigurations;
 import com.clemble.casino.goal.service.GoalConfigurationService;
+import com.clemble.casino.money.Currency;
+import com.clemble.casino.money.Money;
+import com.clemble.casino.rule.bet.LimitedBetRule;
+import com.clemble.casino.rule.breach.LooseBreachPunishment;
+import com.clemble.casino.rule.privacy.PrivacyRule;
+import com.clemble.casino.rule.time.MoveTimeRule;
+import com.clemble.casino.rule.time.TotalTimeRule;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.collect.ImmutableList;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import static com.clemble.casino.goal.GoalWebMapping.MY_CONFIGURATIONS;
 import static com.clemble.casino.goal.GoalWebMapping.MY_GOALS_GOAL_STATUS;
@@ -21,10 +33,21 @@ import static com.clemble.casino.web.mapping.WebMapping.PRODUCES;
 @RestController
 public class GoalConfigurationServiceController implements GoalConfigurationService {
 
+    // TODO replace with SMART configurations
+    final private static List<GoalConfiguration> DEFAULT_CONFIGURATIONS = ImmutableList.of(
+        new GoalConfiguration(
+            new Bid(Money.create(Currency.FakeMoney, 50), Money.create(Currency.FakeMoney, 5)),
+            LimitedBetRule.create(5, 50),
+            new MoveTimeRule(TimeUnit.MILLISECONDS.toDays(1), LooseBreachPunishment.getInstance()),
+            new TotalTimeRule(TimeUnit.MILLISECONDS.toDays(7), LooseBreachPunishment.getInstance()),
+            PrivacyRule.everybody
+        )
+    );
+
     @RequestMapping(method = RequestMethod.GET, value = MY_CONFIGURATIONS, produces = PRODUCES)
     @ResponseStatus(HttpStatus.OK)
     public List<GoalConfiguration> getConfigurations() {
-        return null;
+        return DEFAULT_CONFIGURATIONS;
     }
 
 }
