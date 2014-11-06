03b2908c8acb6b36efcae78fbef4702c02567387
Anton O <mavarazy@gmail.com>
2014-11-06 04:16:37 +0200

    Adding simple toString to SocialConnectionData

--- server/player/player-social/src/main/java/com/clemble/casino/server/social/SocialConnectionDataAdapter.java
+++ server/player/player-social/src/main/java/com/clemble/casino/server/social/SocialConnectionDataAdapter.java
@@ -8,6 +8,8 @@ import com.clemble.casino.player.PlayerProfile;
 import com.clemble.casino.server.event.player.SystemPlayerImageChangedEvent;
 import com.clemble.casino.server.event.player.SystemPlayerProfileRegisteredEvent;
 import org.apache.commons.lang3.tuple.Pair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.social.connect.Connection;
 import org.springframework.social.connect.ConnectionData;
 import org.springframework.social.connect.ConnectionFactoryLocator;
@@ -21,93 +23,106 @@ import com.clemble.casino.social.SocialConnectionData;
 import com.clemble.casino.server.event.player.SystemPlayerSocialAddedEvent;
 import com.clemble.casino.server.player.notification.SystemNotificationService;
 import com.google.common.collect.ImmutableSet;
-import org.springframework.social.connect.web.SignInAdapter;
-import org.springframework.web.context.request.NativeWebRequest;
-
-import javax.servlet.http.HttpServletResponse;
 
 public class SocialConnectionDataAdapter {
+
+    final private static Logger LOG = LoggerFactory.getLogger(SocialConnectionData.class);
 
     final private ConnectionFactoryLocator connectionFactoryLocator;
     final private UsersConnectionRepository usersConnectionRepository;
     final private SocialConnectionAdapterRegistry socialAdapterRegistry;
-    final private SystemNotificationService systemNotificationService;
+    final private SystemNotificationService notificationService;
 
     public SocialConnectionDataAdapter(
             final ConnectionFactoryLocator connectionFactoryLocator,
             final UsersConnectionRepository usersConnectionRepository,
             final SocialConnectionAdapterRegistry socialAdapterRegistry,
-            final SystemNotificationService systemNotificationService) {
+            final SystemNotificationService notificationService) {
         this.connectionFactoryLocator = checkNotNull(connectionFactoryLocator);
         this.usersConnectionRepository = checkNotNull(usersConnectionRepository);
         this.socialAdapterRegistry = checkNotNull(socialAdapterRegistry);
-        this.systemNotificationService = checkNotNull(systemNotificationService);
+        this.notificationService = checkNotNull(notificationService);
     }
 
     public String register(SocialAccessGrant accessGrant) {
+        LOG.debug("register with {}", accessGrant);
         // Step 1. Sanity check
         if (accessGrant == null)
             throw ClembleCasinoException.fromError(ClembleCasinoError.SocialConnectionInvalid);
         // Step 2. Creating ConnectionData
         ConnectionData connectionData = socialAdapterRegistry.getSocialAdapter(accessGrant.getProvider()).toConnectionData(accessGrant);
+        LOG.debug("register normalized to {}", connectionData);
         // Step 3. Registering with fetched Connection data
         return register(connectionData);
     }
 
     public String register(SocialConnectionData socialConnectionData) {
+        LOG.debug("register with {}", socialConnectionData);
         // Step 1. Sanity check
         if (socialConnectionData == null)
             throw ClembleCasinoException.fromError(ClembleCasinoError.SocialConnectionInvalid);
         // Step 3. Converting SocialConnectionData to ConnectionData in accordance with the provider
         ConnectionData connectionData = socialAdapterRegistry.getSocialAdapter(socialConnectionData.getProviderId()).toConnectionData(socialConnectionData);
+        LOG.debug("register normalized to {}", connectionData);
         // Step 4. Registering with fetched connection data
         return register(connectionData);
     }
 
     public String register(ConnectionData connectionData) {
-        SocialConnection socialConnection = null;
         // Step 1. Checking if user already exists
+        LOG.debug("register search for existing users {}", connectionData);
         Set<String> existingUsers = usersConnectionRepository.findUserIdsConnectedTo(connectionData.getProviderId(), ImmutableSet.<String> of(connectionData.getProviderUserId()));
+        LOG.debug("register fetched {}", existingUsers);
+        if (existingUsers.size() > 1)
+            throw ClembleCasinoException.fromError(ClembleCasinoError.SocialConnectionInvalid);
         if (existingUsers.size() == 1) {
+            String player = existingUsers.iterator().next();
+            LOG.debug("register {} already exists {}, verifying connection", player, connectionData);
             // Step 2. Creating connection appropriate for the provided data
             Connection<?> connection = connectionFactoryLocator.getConnectionFactory(connectionData.getProviderId()).createConnection(connectionData);
             // Step 5. Checking if the provided connection was valid
+            LOG.debug("register {} connection obtained for {}, testing if it's valid", player, connection.getKey());
             // TODO need to check that this access was provided for this application
             if (connection.test()) {
+                LOG.debug("register {} test passed for {}, updating connection", player, connection.getKey());
                 // Step 6. Retrieving associated user identifiers
-                String player = existingUsers.iterator().next();
-                socialConnection = new SocialConnection(player, connection);
                 ConnectionRepository connectionRepository = usersConnectionRepository.createConnectionRepository(player);
                 connectionRepository.updateConnection(connection);
-                return existingUsers.iterator().next();
+                return player;
             } else {
+                LOG.error("register {} test FAILED for {}, throwing exception", player, connection.getKey());
                 throw ClembleCasinoException.fromError(ClembleCasinoError.SocialConnectionInvalid);
             }
-        } else if (existingUsers.size() == 0) {
-            // Step 3. Creating connection appropriate for the provided
-            Connection<?> connection = connectionFactoryLocator.getConnectionFactory(connectionData.getProviderId()).createConnection(connectionData);
-            // Step 4. Saving connection in the Database
-            // This is done by calling UserConnectionRespository, which invokes ConnectionSignUp if there is no user with provided Connection
-            // So The new user will be created and added to the DB, the Id will be actually generated by JdbcUsersConnectionRepository
-            // Check that this logic remains intact
-            String player = usersConnectionRepository.findUserIdsWithConnection(connection).iterator().next();
-            socialConnection = new SocialConnection(player, connection);
-        } else {
-            throw ClembleCasinoException.fromError(ClembleCasinoError.ServerCriticalError);
         }
+        LOG.error("register new player from {}:{}", connectionData.getProviderId(), connectionData.getProviderUserId());
+        // Step 3. Creating connection appropriate for the provided
+        Connection<?> connection = connectionFactoryLocator.getConnectionFactory(connectionData.getProviderId()).createConnection(connectionData);
+        LOG.error("register connection acquired for {}", connection.getKey());
+        // Step 4. Saving connection in the Database
+        // This is done by calling UserConnectionRespository, which invokes ConnectionSignUp if there is no user with provided Connection
+        // So The new user will be created and added to the DB, the Id will be actually generated by JdbcUsersConnectionRepository
+        // Check that this logic remains intact
+        // TODO add same verification for connection as above
+        String player = usersConnectionRepository.findUserIdsWithConnection(connection).iterator().next();
+        LOG.error("register {} created with connection {}", player, connection.getKey());
         // Step 5. Fetching player profile
-        SocialConnectionAdapter adapter = socialAdapterRegistry.getSocialAdapter(socialConnection.getConnection().getKey().getProviderId());
-        PlayerProfile playerProfile = adapter.fetchPlayerProfile(socialConnection.getConnection().getApi());
-        playerProfile.setPlayer(socialConnection.getPlayer());
+        SocialConnectionAdapter adapter = socialAdapterRegistry.getSocialAdapter(connection.getKey().getProviderId());
+        PlayerProfile playerProfile = adapter.fetchPlayerProfile(connection.getApi());
+        playerProfile.setPlayer(player);
+        LOG.error("register {} created player profile {}", player, playerProfile);
+        notificationService.send(new SystemPlayerProfileRegisteredEvent(playerProfile.getPlayer(), playerProfile));
         // Step 6. Notifying of added social connection
-        systemNotificationService.send(new SystemPlayerSocialAddedEvent(socialConnection.getPlayer(), socialConnection.getConnection().getKey()));
-        Pair<String, String> imageUrl = adapter.toImageUrl(socialConnection.getConnection());
-        if (imageUrl != null)
-            systemNotificationService.send(new SystemPlayerImageChangedEvent(socialConnection.getPlayer(), imageUrl.getLeft(), imageUrl.getRight()));
+        Pair<String, String> imageUrl = adapter.toImageUrl(connection);
+        LOG.error("register {} updating player picture", player, imageUrl.getLeft());
+        if (imageUrl != null) {
+            notificationService.send(new SystemPlayerImageChangedEvent(player, imageUrl.getLeft(), imageUrl.getRight()));
+        }
+        LOG.error("register {} notifying social added to update connections", player, imageUrl.getLeft());
         // Step 8. Sending notification for registered social profile
-        systemNotificationService.send(new SystemPlayerProfileRegisteredEvent(playerProfile.getPlayer(), playerProfile));
+        notificationService.send(new SystemPlayerSocialAddedEvent(player, connection.getKey()));
         // Step 9. Returning player profile
-        return socialConnection.getPlayer();
+        LOG.error("register {} returning", player);
+        return player;
     }
 
     public SocialConnectionData add(String player, SocialConnectionData socialConnectionData) {
@@ -128,7 +143,7 @@ public class SocialConnectionDataAdapter {
         // Step 4. Adding connection to player connection repository
         usersConnectionRepository.createConnectionRepository(player).addConnection(connection);
         // Step 5. Sending request to update user connections
-        systemNotificationService.send(new SystemPlayerSocialAddedEvent(player, connection.getKey()));
+        notificationService.send(new SystemPlayerSocialAddedEvent(player, connection.getKey()));
         // Step 6. Returning original social connection data
         return socialConnectionData;
     }
