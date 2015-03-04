57a808734c1d0ecff4666b6d0cb5bc8e4a0d7588
Anton O <mavarazy@gmail.com>
2015-03-04 20:58:41 +0200

    Tuning configurations

--- server/goal/goal-configuration/src/test/java/com/clemble/casino/goal/configurationn/controller/GoalConfigurationServiceControllerTest.java
+++ server/goal/goal-configuration/src/test/java/com/clemble/casino/goal/configurationn/controller/GoalConfigurationServiceControllerTest.java
@@ -3,6 +3,8 @@ package com.clemble.casino.goal.configurationn.controller;
 import com.clemble.casino.goal.lifecycle.configuration.GoalConfiguration;
 import com.clemble.casino.goal.configuration.controller.GoalConfigurationServiceController;
 import com.clemble.casino.goal.configuration.spring.GoalConfigurationSpringConfiguration;
+import com.clemble.casino.goal.lifecycle.configuration.GoalConfigurationChoices;
+import com.clemble.casino.goal.lifecycle.configuration.IntervalGoalConfigurationBuilder;
 import com.clemble.casino.json.ObjectMapperUtils;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Assert;
@@ -27,11 +29,28 @@ public class GoalConfigurationServiceControllerTest {
     public GoalConfigurationServiceController configurationServiceController;
 
     @Test
-    public void test() throws IOException {
+    public void testConfigurationsRead() throws IOException {
         ObjectMapper objectMapper = ObjectMapperUtils.OBJECT_MAPPER;
         String configurationSerialized = objectMapper.writeValueAsString(configurationServiceController.getConfigurations());
         Collection<GoalConfiguration> readConfigurations =  Arrays.asList(objectMapper.readValue(configurationSerialized, GoalConfiguration[].class));
         Assert.assertEquals(readConfigurations, configurationServiceController.getConfigurations());
+    }
+
+    @Test
+    public void testChoiceRead() throws IOException {
+        ObjectMapper objectMapper = ObjectMapperUtils.OBJECT_MAPPER;
+        String choiceSerialized = objectMapper.writeValueAsString(configurationServiceController.getChoises());
+        GoalConfigurationChoices readChoices =  objectMapper.readValue(choiceSerialized, GoalConfigurationChoices.class);
+        Assert.assertEquals(readChoices, configurationServiceController.getChoises());
+
+    }
+
+    @Test
+    public void testIntervalRead() throws IOException {
+        ObjectMapper objectMapper = ObjectMapperUtils.OBJECT_MAPPER;
+        String intervalSerialized = objectMapper.writeValueAsString(configurationServiceController.getIntervalBuilder());
+        IntervalGoalConfigurationBuilder intervalConfigurations =  objectMapper.readValue(intervalSerialized, IntervalGoalConfigurationBuilder.class);
+        Assert.assertEquals(intervalConfigurations, configurationServiceController.getIntervalBuilder());
     }
 
 }
